823bf0a767ef466c6503beb402778d28ed347d02
Ricardo Alcantara <alcantararicardo04@gmail.com>
2024-10-15 17:49:01 -0500

    EmailConfig: se configura bean ResourceLoader

--- mail/src/main/java/com/sending/mail/config/EmailConfig.java
+++ mail/src/main/java/com/sending/mail/config/EmailConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
@@ -37,5 +39,9 @@ public class EmailConfig {
         javaMailSenderimpl.setUsername(email);
         javaMailSenderimpl.setPassword(password);
         return javaMailSenderimpl;
+    }
+    @Bean
+    public ResourceLoader resourceLoader(){
+        return new DefaultResourceLoader();
     }
 }
