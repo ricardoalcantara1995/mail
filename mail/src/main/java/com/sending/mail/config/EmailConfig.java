f663163e81e59bff64f6ed29eecc423ff21ccbbf
Ricardo Alcantara <alcantararicardo04@gmail.com>
2024-10-15 16:20:21 -0500

    EmailConfig: indicando que use el archivo email.properties para las variables

--- mail/src/main/java/com/sending/mail/config/EmailConfig.java
+++ mail/src/main/java/com/sending/mail/config/EmailConfig.java
@@ -1,4 +1,9 @@
 package com.sending.mail.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:email.properties")
 public class EmailConfig {
 }
