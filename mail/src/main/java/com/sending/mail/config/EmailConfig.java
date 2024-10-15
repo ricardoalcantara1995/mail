d51786d9ca7b48bfdf79f1aa7105929f3666818d
Ricardo Alcantara <alcantararicardo04@gmail.com>
2024-10-15 17:45:32 -0500

    EmailConfig: se configura bean JavaMailSender para mandar correos

--- mail/src/main/java/com/sending/mail/config/EmailConfig.java
+++ mail/src/main/java/com/sending/mail/config/EmailConfig.java
@@ -1,8 +1,12 @@
 package com.sending.mail.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
 import java.util.Properties;
 
 @Configuration
@@ -13,13 +17,25 @@ public class EmailConfig {
     @Value("${email.password}")
     private String password;
 
-    private Properties getMailProperties(){
+    public Properties getMailProperties(){
         //Instanciamos la clase Properties
         Properties properties = new Properties();
+        //Activa la autenticación SMTP.
         properties.put("mail.smtp.auth","true");
+        //Habilita el uso de TLS para enviar correos de manera segura
         properties.put("mail.smtp.starttls.enable","true");
+        //Especifica el servidor SMTP de Gmail.
         properties.put("mail.smtp.host","smtp.gmail.com");
+        //El puerto 587 es el puerto estándar para enviar correos usando TLS en Gmail.
         properties.put("mail.smtp.port","587");
         return properties;
+    }
+    @Bean
+    public JavaMailSender javaMailSender(){
+        JavaMailSenderImpl javaMailSenderimpl = new JavaMailSenderImpl();
+        javaMailSenderimpl.setJavaMailProperties(getMailProperties());
+        javaMailSenderimpl.setUsername(email);
+        javaMailSenderimpl.setPassword(password);
+        return javaMailSenderimpl;
     }
 }
