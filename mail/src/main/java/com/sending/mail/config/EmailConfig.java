4522f78f96a82272f1d3992cb500af96d1488e16
Ricardo Alcantara <alcantararicardo04@gmail.com>
2024-10-15 16:40:06 -0500

    EmailConfig: Setiamos los atributos email y password , creamos getMailProperties que retorna properties configurado

--- mail/src/main/java/com/sending/mail/config/EmailConfig.java
+++ mail/src/main/java/com/sending/mail/config/EmailConfig.java
@@ -1,9 +1,25 @@
 package com.sending.mail.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import java.util.Properties;
 
 @Configuration
 @PropertySource("classpath:email.properties")
 public class EmailConfig {
+    @Value("${email.username}")
+    private String email;
+    @Value("${email.password}")
+    private String password;
+
+    private Properties getMailProperties(){
+        //Instanciamos la clase Properties
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth","true");
+        properties.put("mail.smtp.starttls.enable","true");
+        properties.put("mail.smtp.host","smtp.gmail.com");
+        properties.put("mail.smtp.port","587");
+        return properties;
+    }
 }
