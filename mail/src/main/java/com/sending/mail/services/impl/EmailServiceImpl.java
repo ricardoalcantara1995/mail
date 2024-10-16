f23b3268d957db76c39148aee60fb77472c04d99
Ricardo Alcantara <alcantararicardo04@gmail.com>
2024-10-15 19:58:24 -0500

    EmailServiceImpl: agregando inyección de dependencias

--- mail/src/main/java/com/sending/mail/services/impl/EmailServiceImpl.java
+++ mail/src/main/java/com/sending/mail/services/impl/EmailServiceImpl.java
@@ -1,3 +1,15 @@
 package com.sending.mail.services.impl;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+
+@Service
+@Data
 public class EmailServiceImpl {
+    @Autowired
+    private final JavaMailSender javaMailSender;
+    private final TemplateEngine templateEngine;
 }
