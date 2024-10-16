551e769eb2b52f881bb2026cd6615b965eae7790
Ricardo Alcantara <alcantararicardo04@gmail.com>
2024-10-15 20:46:43 -0500

    sendMail: metodo hace el envio de correo con plantilla thymeleaf

--- mail/src/main/java/com/sending/mail/services/impl/EmailServiceImpl.java
+++ mail/src/main/java/com/sending/mail/services/impl/EmailServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @Service
 @Data
@@ -20,8 +21,23 @@ public class EmailServiceImpl implements IEmailServices {
 
     @Override
     public void sendMail(EmailDTO emailDTO) throws MessagingException {
+    try{
         MimeMessage message = javaMailSender.createMimeMessage();
         MimeMessageHelper helper =
                 new MimeMessageHelper(message,true,"UTF-8");
-    }
+        helper.setTo(emailDTO.getRecipient());
+        helper.setSubject(emailDTO.getSubject());
+        //Contexto de thymeleaf
+        Context context = new Context();
+        context.setVariable("message",emailDTO.getMessage());
+        String ContentHTML =
+                //nombre de la pantilla
+                templateEngine.process("email",context);
+        helper.setText(ContentHTML,true);
+        javaMailSender.send(message);
+    }catch (Exception exception){
+            throw new RuntimeException("Error Email has not send " +
+                    ""+exception.getMessage() + " "+exception);
+        }
+            }
 }
