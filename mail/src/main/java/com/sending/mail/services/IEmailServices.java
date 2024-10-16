65ef1f6babe696d1286bebbcce3a8b239107db77
Ricardo Alcantara <alcantararicardo04@gmail.com>
2024-10-15 20:25:19 -0500

    EmailServiceImpl: creando el metodo sendMail

--- mail/src/main/java/com/sending/mail/services/IEmailServices.java
+++ mail/src/main/java/com/sending/mail/services/IEmailServices.java
@@ -1,7 +1,8 @@
 package com.sending.mail.services;
 
 import com.sending.mail.dto.EmailDTO;
+import jakarta.mail.MessagingException;
 
 public interface IEmailServices {
-    public void sendMail(EmailDTO emailDTO);
+    public void sendMail(EmailDTO emailDTO) throws MessagingException;
 }
--- mail/src/main/java/com/sending/mail/services/impl/EmailServiceImpl.java
+++ mail/src/main/java/com/sending/mail/services/impl/EmailServiceImpl.java
@@ -2,9 +2,12 @@ package com.sending.mail.services.impl;
 
 import com.sending.mail.dto.EmailDTO;
 import com.sending.mail.services.IEmailServices;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 
@@ -16,7 +19,9 @@ public class EmailServiceImpl implements IEmailServices {
     private final TemplateEngine templateEngine;
 
     @Override
-    public void sendMail(EmailDTO emailDTO) {
-
+    public void sendMail(EmailDTO emailDTO) throws MessagingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper =
+                new MimeMessageHelper(message,true,"UTF-8");
     }
 }
