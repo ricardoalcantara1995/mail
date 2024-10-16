40fb4af54c92445962cb5846b392ee7cfbbb98d7
Ricardo Alcantara <alcantararicardo04@gmail.com>
2024-10-15 20:04:05 -0500

    EmailServiceImpl: implementando la interfaz IEmailServices para hacer contrato de metodos

--- mail/src/main/java/com/sending/mail/services/IEmailServices.java
+++ mail/src/main/java/com/sending/mail/services/IEmailServices.java
@@ -1,4 +1,7 @@
 package com.sending.mail.services;
 
+import com.sending.mail.dto.EmailDTO;
+
 public interface IEmailServices {
+    public void sendMail(EmailDTO emailDTO);
 }
--- mail/src/main/java/com/sending/mail/services/impl/EmailServiceImpl.java
+++ mail/src/main/java/com/sending/mail/services/impl/EmailServiceImpl.java
@@ -1,5 +1,7 @@
 package com.sending.mail.services.impl;
 
+import com.sending.mail.dto.EmailDTO;
+import com.sending.mail.services.IEmailServices;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -8,8 +10,13 @@ import org.thymeleaf.TemplateEngine;
 
 @Service
 @Data
-public class EmailServiceImpl {
+public class EmailServiceImpl implements IEmailServices {
     @Autowired
     private final JavaMailSender javaMailSender;
     private final TemplateEngine templateEngine;
+
+    @Override
+    public void sendMail(EmailDTO emailDTO) {
+
+    }
 }
